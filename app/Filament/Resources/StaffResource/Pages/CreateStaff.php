571e2cd7d055dbf8a45d38444bda779751ba1339
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2022-06-14 13:42:26 +0100

    create staff resource

---
+++ app/Filament/Resources/StaffResource/Pages/CreateStaff.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\StaffResource\Pages;
+
+use App\Filament\Resources\StaffResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStaff extends CreateRecord
+{
+    protected static string $resource = StaffResource::class;
+}
---
+++ app/Filament/Resources/StaffResource/Pages/EditStaff.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\StaffResource\Pages;
+
+use App\Filament\Resources\StaffResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditStaff extends EditRecord
+{
+    protected static string $resource = StaffResource::class;
+}
